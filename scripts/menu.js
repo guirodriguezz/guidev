16b71359d928f5b369e6e2fba8a92ffc704bc17d
Guilherme Alexandre Marques <gui.rodrigues@msn.com>
2021-01-26 09:20:13 -0300

    Feature/criacao css (#24)
    * Adicionando menu hamburguer no mobile.
    
    * Adicionando ação de ativar e desativar menu.
    
    * Organizando pastas e minimizando css.
    
    * Adicionando funções de animação com javascript

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,16 @@
+const sidenavMenu = document.querySelector(".sidenav__menu");
+
 function menuShow() {
-  var menu = document.querySelectorAll(".menu--disabled");
+  var menu = document.querySelectorAll(".sidenav--hamburguer");
+  var liAnimate = document.querySelectorAll("li.animate__animated")
+
   menu.forEach((item) => {
-    item.classList.toggle("menu--enabled");
+    item.classList.toggle("sidenav__menu--enabled");
+  });
+
+  liAnimate.forEach((item) => {
+    item.classList.toggle("animate__slideInLeft");
   });
 }
+
+sidenavMenu.addEventListener('click', menuShow);
