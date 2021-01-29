f240f653a44e5bda3b9e991417252ecb87320032
Guilherme Rodrigues <gui.rodrigues@msn.com>
2021-01-29 09:32:19 -0300

    Adicionando dark mode.

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,7 +2,7 @@ const sidenavMenu = document.querySelector(".sidenav__menu");
 
 function menuShow() {
   var menu = document.querySelectorAll(".sidenav--hamburguer");
-  var liAnimate = document.querySelectorAll("li.animate__animated")
+  var liAnimate = document.querySelectorAll("li.animate__animated");
 
   menu.forEach((item) => {
     item.classList.toggle("sidenav__menu--enabled");
@@ -13,4 +13,21 @@ function menuShow() {
   });
 }
 
-sidenavMenu.addEventListener('click', menuShow);
+sidenavMenu.addEventListener("click", menuShow);
+
+const sidenavNoturno = document.querySelector(".sidenav__noturno");
+
+function darkMode() {
+  var html = document.querySelector("html");
+  var introducaoDark = document.querySelector(".introducao__sampa--dark");
+  var introducaoWhite = document.querySelector(".introducao__sampa--white");
+
+  html.classList.toggle("dark__mode");
+
+  introducaoDark.classList.replace("introducao__sampa--dark", "introducao__sampa--white");
+  introducaoWhite.classList.replace("introducao__sampa--white", "introducao__sampa--dark");
+
+  sidenavNoturno.classList.toggle("fa-sun");
+}
+
+sidenavNoturno.addEventListener("click", darkMode);
